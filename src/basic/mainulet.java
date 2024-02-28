0d030bcc91842d8b76b598dec0bccafc146f769e
Coșa Andrei Valeriu <42703353+AndreiOp235@users.noreply.github.com>
2024-02-28 12:26:30 +0200

    init

---
+++ src/basic/mainulet.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class mainulet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+	}
+
+}
---
+++ src/readme.md
@@ -0,0 +1,3 @@
+Hello
+*bau
+#titlu
